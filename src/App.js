a2addbc4315a2a8f23cd3618ebd7f71dad697c18
Harish <harish.thavva@gmail.com>
2021-05-09 18:56:25 +0530

    Timer Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React,{Component} from 'react';
-import Particles from 'react-particles-js';
 import tachyons from "tachyons";
 import Input from "./Input";
 import "./index.css";
 
 class App extends Component{
-   date= new Date(); 
-render(){
-  return(
-    <div className="tc modify">
-      <h2>{`Current Time ${this.date}`}</h2>
-      <h4>Please Enter the date on which the Timer Ends !!!</h4>
-      <Input />
-      <span> </span>
-      <button className="pa2 bg-light-grey pointer" type="submit">Submit</button>
-    </div>
-  )
+  date= new Date()
+  constructor(){
+    super()
+    this.state={
+      searchFiled:"",
+      days: 0,
+      hours: 0,
+      minutes: 0, 
+      seconds: 0,
+    }
+  }
   
-}
 
-}
+  onChange=(e)=>{
+    //console.log(e.target.value)
+    this.setState({searchFiled: e.target.value})
+  }
+
+  Timer =()=>{
+    //console.log("Hi")
+    console.log(this.state.searchFiled)
+    var countTime=new Date(this.state.searchFiled).getTime()
+    var x= setInterval(()=>{
+      var now = new Date().getTime();
+      var distance=countTime-now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      console.log(hours)
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      //document.getElementById("timer").innerHTML=`${hours}Hours ${minutes}Minutes ${seconds}Seconds`
+      this.setState({days:days,hours:hours,minutes:minutes,seconds:seconds})
+      console.log(`Timer Ends in this time period :- ${days}Days ${hours}Hours ${minutes}Minutes ${seconds}Seconds`)
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+      },1000)
+
+  }
+
+  render(){
+    return(
+      <div className="tc modify">
+        <h2>{`Current Time ${this.date}`}</h2>
+        <h4>Please Enter the date on which the Timer Ends !!!</h4>
+        <h2>{`Remaining Time Period :- ${this.state.days}Days ${this.state.hours}Hours ${this.state.minutes}Minutes ${this.state.seconds}Seconds`}</h2>
+        <input className="pa2 bg-light-green" type="date" placeholder="Enter date in MM-DD-YEAR Formate" onChange={this.onChange}/>
+        <span> </span>
+        <button className="pa2 bg-light-grey pointer" type="submit" onClick={this.Timer}>Submit</button>
+      </div>
+    )
+    
+  }
+
+  }
 
 
 
--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,7 @@
 
-const Input = ()=>{
+const Input = (searchBox)=>{
    return (
-    <input className="pa2 bg-light-green" type="text" placeholder="Enter date in MM-DD-YEAR Formate"/>
+    <input className="pa2 bg-light-green" type="text" placeholder="Enter date in MM-DD-YEAR Formate" onChange={searchBox}/>
    ) 
 }
 
